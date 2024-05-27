7479ea71374509af93185082a6bc3442e1a6810e
maicoltabares <maicoltabares65@gmail.com>
2024-05-27 15:22:30 -0500

    add-messages-scene

--- app/scenes/private/messages/messages.js
+++ app/scenes/private/messages/messages.js
@@ -1,11 +1,50 @@
 import styles from './messages.css';
+import property_AA_3401_10 from "../../../assets/images/imgFace.jpg";
 
 export async function messagesScene() {
   const root = document.getElementById('root');
 
   root.innerHTML = `
-    //Aqui se pone el codigo HTML llamando style.nombredelaclase para poder darle estilos
-    
+    <div class="${styles.chatCenter}">  
+        <div class="${styles.chatContainer}">
+            <div class="${styles.headerBar}">
+                <div class="${styles.imgChat}">
+                    <img src="${property_AA_3401_10}" class="${styles.profilePicture}">
+                </div>
+                <div class="${styles.buttonClose}">
+                    <button class="${styles.closeButton}">X</button>
+                </div>
+            </div>
+            <div class="${styles.agentMessages}">
+                <div class="${styles.agentMessage}">
+                    <div class="${styles.nameText}">
+                        <h4 class="${styles.username} ">Mariana Pérez</h4>
+                    </div>
+                    <p>¡Hola, Dilan!</p>
+                    <p class="${styles.textChat}">Estoy interesado en la casa</p>
+                </div>
+            </div>
+            <div class="${styles.userMessages}">
+                <div class="${styles.userMessage}">
+                    <h4 class="${styles.username}">Dilan Numa</h4>
+                    <p>¡Hola!</p>
+                    <p>dime como te puedo ayudar?</p>
+                </div>
+            </div>
+            <div class="${styles.agentMessages}">
+                <div class="${styles.agentMessage}">
+                    <div class="${styles.nameText}">
+                        <h4 class="${styles.username} ">Mariana Pérez</h4>
+                    </div>
+                    <p>Quiero más fotos de la propiedad</p>
+                </div>
+            </div>
+            <form class="${styles.messageForm}">
+                <input type="text" class="${styles.messageInput}" placeholder="Escribe tu mensaje...">
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    </div>
     `;
 
   //Aqui se pone la logica de esta pagina, botones que redirigen u cualquier otra cosa
